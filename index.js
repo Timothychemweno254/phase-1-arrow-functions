16bc5feb3fc8dcca42e11d5475900de22f8b5893
timothychemweno254 <timothy.kiprop@student.moringaschool.com>
2025-03-06 12:08:21 +0300

    arrithmeticss

---
+++ index.js
@@ -0,0 +1,10 @@
+// divide function expression
+const divide = function (dividend, divisor) {
+    return dividend / divisor;
+  };
+  
+  // square arrow function
+  const square = (number) => number * number;
+  
+  // add arrow function
+  const add = (num1, num2) => num1 + num2;
